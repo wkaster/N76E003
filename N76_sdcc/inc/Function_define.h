9edc7f976d1edecb4bdb797a1304f54b5db88c95
Wiliam Kaster <wkaster@localhost.localdomain>
2019-09-30 14:56:45 -0300

    Added 16.6MHz Timer definitions

--- N76_sdcc/inc/Function_define.h
+++ N76_sdcc/inc/Function_define.h
@@ -235,6 +235,24 @@ All function define inital setting file for Nuvoton N76E003
 		#define TIMER_DIV256_VALUE_500ms		65536-31250	//31250*256/16000000 = 500 ms 	// Timer divider = 256
 		#define	TIMER_DIV512_VALUE_1s				65536-31250	//31250*512/16000000 = 1 s.  		// Timer Divider = 512
 #endif
+#ifdef FOSC_166000		// if Fsys = 16.6MHz 
+		#define TIMER_DIV12_VALUE_10us			65536-13		//13*12/16600000 = 10 uS,  			// Timer divider = 12 for TM0/TM1
+		#define TIMER_DIV12_VALUE_100us			65536-138		//138*12/16600000 = 100 uS,  		// Timer divider = 12 
+		#define TIMER_DIV12_VALUE_1ms			65536-1384	//1384*12/16600000 = 1 mS, 			// Timer divider = 12 
+		#define TIMER_DIV12_VALUE_10ms			65536-13834	//13834*12/16600000 = 10 mS 		// Timer divider = 12 
+		#define TIMER_DIV12_VALUE_40ms			65536-55334	//55334*12/16600000 = 40 ms			// Timer divider = 12 
+		#define TIMER_DIV4_VALUE_10us			65536-41		//41*4/16600000 = 10 uS,    		// Timer divider = 4	for TM2/TM3
+		#define TIMER_DIV4_VALUE_100us			65536-415		//415*4/16600000 = 100 us				// Timer divider = 4
+		#define TIMER_DIV4_VALUE_200us			65536-830		//830*4/16600000 = 200 us				// Timer divider = 4
+		#define TIMER_DIV4_VALUE_500us			65536-2075	//2075*4/16600000 = 500 us			// Timer divider = 4
+		#define TIMER_DIV4_VALUE_1ms			65536-4150	//4150*4/16600000 = 1 mS,   		// Timer divider = 4
+		#define TIMER_DIV16_VALUE_10ms			65536-10375	//10375*16/16600000 = 10 ms			// Timer	divider = 16
+		#define TIMER_DIV64_VALUE_30ms			65536-7781	//7781*64/16600000 = 30 ms			// Timer divider = 64
+		#define	TIMER_DIV128_VALUE_100ms		65536-12969	//12969*128/16600000 = 100 ms		// Timer divider = 128
+		#define	TIMER_DIV128_VALUE_200ms		65536-25938	//25938*128/16600000 = 200 ms		// Timer divider = 128
+		#define TIMER_DIV256_VALUE_500ms		65536-32422	//32422*256/16600000 = 500 ms 	// Timer divider = 256
+		#define	TIMER_DIV512_VALUE_1s			65536-32422	//32422*512/16600000 = 1 s.  		// Timer Divider = 512
+#endif
 #ifdef FOSC_184320		// if Fsys = 18.432MHz 
     #define TIMER_DIV12_VALUE_10us			65536-15    //15*12/18.432 = 10 uS,  Timer Clock = Fsys/12
     #define TIMER_DIV12_VALUE_1ms				65536-1536  //1536*12/18.432 = 1 mS, Timer Clock = Fsys/12
