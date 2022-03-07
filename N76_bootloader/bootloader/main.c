b34a5ff9442c74293113aee0b315ac1dbabae361
Konstantin Kim <konstantin.kim@ellisys.com>
2022-03-07 19:29:07 +0100

    footprint 508 bytes. untested, perhaps screwed :)

--- N76_bootloader/bootloader/main.c
+++ N76_bootloader/bootloader/main.c
@@ -28,7 +28,7 @@
 
 uint8_t idx;
 uint8_t timerCount;
-uint8_t receivedBuf[20];
+uint8_t buff[BLOCK_SIZE +3];
 
 uint16_t __at (0xA6) IAPA16 ;
 
@@ -51,7 +51,7 @@ inline void uart_init(uint32_t u32Baudrate) //use timer3 as Baudrate generator
 	RL3    = LOBYTE(65536 - (1037500/u32Baudrate)); 		/*16.6 MHz */
 #endif
 	set_TR3;         //Trigger Timer3
-	set_TI;					 //For printf function must setting TI = 1
+	set_TI;			 //For printf function must setting TI = 1
 }
 
 inline void tx_sync(){
@@ -62,6 +62,7 @@ static void tx(char c) {
 	tx_sync();
 	TI = 0;
 	SBUF = c;
+	idx = 0;
 }
 
 static void ta() {	//timed access protection
@@ -70,8 +71,8 @@ static void ta() {	//timed access protection
 }
 
 static void iap_on() {	//warning:  interrupts have to be disabled
-	ta();	CHPCON|=SET_BIT0;	//set_IAPEN; 		//enable IAP mode
-	ta();	IAPUEN|=SET_BIT0;	//set_APUEN;		//enable APROM update
+	ta();	CHPCON |= SET_BIT0;	//set_IAPEN; 		//enable IAP mode
+	ta();	IAPUEN |= SET_BIT0;	//set_APUEN;		//enable APROM update
 }
 
 static void iap_go() {
@@ -79,31 +80,30 @@ static void iap_go() {
 }
 
 static void iap_off() {
-	ta();	IAPUEN&=~SET_BIT0;	//	clr_APUEN;
-	ta();	CHPCON&=~SET_BIT0;	//	clr_IAPEN;
+	ta();	IAPUEN &=~ SET_BIT0;	//	clr_APUEN;
+	ta();	CHPCON &=~ SET_BIT0;	//	clr_IAPEN;
 }
 
 void timer0_isr(void) __interrupt 1
 {
 	TL0 = LOBYTE(TIMER_DIV12_VALUE_40ms);
 	TH0 = HIBYTE(TIMER_DIV12_VALUE_40ms);
-	if (!(++timerCount&0x0f))	
+	if (!(++timerCount&0x0f))
 	{
 		clr_TR0;  // stop timer
 		idx = 1;
-		receivedBuf[0] = CMD_EOT;	// reset from aprom
+		buff[0] = CMD_EOT;	// reset from aprom
 	}
 }
 
 void serial_isr() __interrupt 4
 {
-	if(RI == 1)	{
-		receivedBuf[idx] = SBUF;
-		idx++;
-		if(idx > sizeof(receivedBuf)-1) {
-			receivedBuf[0] = 0x00;
+	if (RI)	{
+		if (idx >= sizeof(buff)) {	//overflow
 			idx = 0;
 		}
+		buff[idx] = SBUF;
+		idx++;
 		RI = 0;              // Clear the Receive interrupt flag
 	}
 }
@@ -129,7 +129,6 @@ inline uint8_t dallas_crc8(const __idata uint8_t* data, uint8_t size)
 
 void main()
 {
-	uint8_t  crc8 = 0;
 //	uint16_t u16ApromAddr = 0x0000;
 	u16ApromAddr = 0x0000;
 	bool cmdmode = true;
@@ -152,14 +151,13 @@ void main()
 	for(;;)	{
 		// commands
 		if(idx > 0 && cmdmode) {
-			switch(receivedBuf[0])
+			switch(buff[0])
 			{
 				case CMD_SOH:
 					tx(CMD_ACK);
 					clr_TR0;						// stop timer
-					idx = 0;
 					cmdmode = true;
-				break;
+					break;
 				case CMD_EOT:
 					tx(CMD_ACK);
 					tx_sync();
@@ -167,64 +165,54 @@ void main()
 					clr_EA;							// disable interrupts
 					ta();	CHPCON&=~SET_BIT1;		// clr_BS		boot from APROM
 					ta();	CHPCON|= SET_BIT7;		// set_SWRST	reset
-				break;
+					break;
 				case CMD_STX:
 					cmdmode = false;
 					break;
 				case CMD_SUB:
 				{
-					if(idx == 2 && receivedBuf[1] == CMD_DEL) {
-						// Erase APROM (128 bytes per page)
+					if(idx == 2 && buff[1] == CMD_DEL) {	// Erase APROM (128 bytes per page)
+						uint8_t i;
 						clr_EA;								// disable interrupts
 						iap_on();
 
-						uint16_t dst;
-#if APROM_SIZE>32767
-						for(dst=0 ; dst < APROM_SIZE ; dst += 128) {
-#else
-						uint8_t i;
-						dst = 0x0000;
-						for(i = APROM_SIZE/128 ; i ; dst += 128, --i)	{
-#endif
-							IAPA16 = dst;
-							IAPFD = 0xFF;						// this mode must be 0xFF (TODO: may we move it out of loop?)
 							IAPCN = 0x22;						// APROM page erase - see datasheet IAP modes and command codes
-							iap_go();
-						}
+							IAPFD = 0xFF;						// this mode must be 0xFF
+							IAPA16 = 0;
+							for(i = APROM_SIZE/256 ; i ; --i )	{
+								iap_go();	IAPAL = 0x80;
+								iap_go();	IAPAL = 0x00;
+							}
 
 						iap_off();
 						u16ApromAddr = 0x0000;				// reset Aprom Address
 						set_EA;								// enable interrupts
 						tx(CMD_ACK);
-						idx = 0;
 						break;
 					}
 					//here fall down to default exception
 				}
 				default:
 					tx(CMD_NACK);
-					idx = 0;
 					break;
 			}
 		}
 		// data
-		if(idx > 17 && !cmdmode) {
-			if(receivedBuf[18] == CMD_ETX) {
-				crc8 = dallas_crc8(&receivedBuf[1], BLOCK_SIZE);
-				if(crc8 == receivedBuf[BLOCK_SIZE + 1]) {
+		if( (idx >= BLOCK_SIZE+2) && !cmdmode) {
+			if(buff[BLOCK_SIZE +2] == CMD_ETX) {
+				uint8_t crc = dallas_crc8(&buff[1], BLOCK_SIZE);
+				if(buff[BLOCK_SIZE + 1] == crc) {
 					uint8_t i;
-					const __idata uint8_t*	src = receivedBuf;
+					const __idata uint8_t*	src = buff;
 					//Save data to APROM DATAFLASH
 					clr_EA;								// disable interrupts
 					iap_on();
-					for(i = BLOCK_SIZE; i; --i) {
-						//IAPAL = u16ApromAddr&0xff;			// low byte
-						//IAPAH = (u16ApromAddr>>8)&0xff;		// high byte
-						IAPFD = *++src;						// byte to save
-						IAPCN = 0x21;						// APROM byte program - see datasheet IAP modes and command codes (TODO: may we move it out of loop?)
-						iap_go();							// do it!
-						u16ApromAddr++;						// next APROM byte addr
-					}
+						IAPCN = 0x21;						// APROM byte program - see datasheet IAP modes and command codes
+						for(i = BLOCK_SIZE; i; --i) {
+							IAPFD = *++src;						// byte to save
+							iap_go();							// do it!
+							u16ApromAddr++;						// next APROM byte addr
+						}
 					iap_off();
 					set_EA;								// enable interrupts
 					tx(CMD_ACK);
@@ -232,7 +220,6 @@ void main()
 				else {
 					tx(CMD_NACK);
 				}
-				idx = 0;
 				cmdmode = true;
 			}
 		}
